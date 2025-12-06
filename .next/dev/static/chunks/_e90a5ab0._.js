58bc081af4f35a918f3a0ee35414c314ffd63cd8
lumavastudio-hub <lumavastudio@gmail.com>
2025-12-06 13:56:06 -0500

    Fix issue

--- .next/dev/static/chunks/_e90a5ab0._.js
+++ .next/dev/static/chunks/_e90a5ab0._.js
@@ -176,7 +176,7 @@ function Hero() {
                 poster: "/videos/hero-fallback.jpg",
                 children: [
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("source", {
-                        src: "/videos/hero.m4v",
+                        src: "/videos/hero.mp4",
                         type: "video/mp4"
                     }, void 0, false, {
                         fileName: "[project]/components/Hero.tsx",
